ab2d5159026be78929a4985d843869632e77042d
onixxx <haxzzzuser@gmail.com>
2025-05-05 19:38:12 +0300

    Almost finished technical part of code

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, useEffect } from "react";
 import "./App.css";
 
-function App() {
+export function App() {
+
   function capitalizeFirstLetter(val: any) {
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
   }
@@ -11,6 +12,27 @@ function App() {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
+
+  const [degrees, setDegrees] = useState<string>("");
+
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+  const API_URL = import.meta.env.VITE_WEATHER_API_URL;
+  const city = text
+  const url = `${API_URL}?key=${API_KEY}&q=${city}&aqi=no`
+
+  useEffect(() => {
+
+    function fetchWeather() {
+      return fetch(url).then(response => response.json())
+    }
+  
+    fetchWeather()
+    .then(data => {
+      setDegrees(data.current.temp_c)
+    })
+    .catch(e => (console.log(e)))
+  }
+)
 
   return (
     <main className="container">
@@ -23,9 +45,10 @@ function App() {
         <h1>
           Погода в {capitalizeFirstLetter(text)}
         </h1>
+        <h2 id="weatherOutput">
+          {degrees}
+        </h2>
       </div>
     </main>
   );
 }
-
-export default App;
